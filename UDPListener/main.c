00411c0a68fc04da904942b7e4179a46580f4311
Matthias Jobst <matthias.jobst@gmx.net>
2021-12-06 22:51:05 +0100

    Initial Commit

---
+++ UDPListener/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  UDPListener
+//
+//  Created by Matthias Jobst on 06.12.21.
+//  Copyright © 2021 Matthias Jobst. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
